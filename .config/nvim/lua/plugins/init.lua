d401df4369dd4eaf1939242fcd1e5c67d7696542
jayfaza <andtv489@gmail.com>
2026-08-26 12:55:44 +0300

    Nvim plugins update

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,229 +1,330 @@
-
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		-- event = 'BufWritePre', -- uncomment for format on save
+		opts = require("configs.conform"),
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
 
-  -- test new blink
-  { import = "nvchad.blink.lazyspec" },
+	-- test new blink
+	{ import = "nvchad.blink.lazyspec" },
 
-  {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css", "rust", "python"
-  		},
-  	},
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim",
+				"lua",
+				"vimdoc",
+				"html",
+				"css",
+				"rust",
+				"python",
+			},
+		},
+	},
 
-  {
-    "nvim-mini/mini.misc",
-    version = false,
-    lazy = false,
-    config = function()
-      require("mini.misc").setup_termbg_sync()
+	{
+		"nvim-mini/mini.misc",
+		version = true,
+		lazy = false,
+		config = function()
+			require("mini.misc").setup_termbg_sync()
+		end,
+	},
+	{
+		"stevearc/overseer.nvim",
+		cmd = {
+			"OverseerRun",
+			"OverseerToggle",
+			"OverseerOpen",
+			"OverseerClose",
+		},
+		opts = {
+			strategy = {
+				"toggleterm",
+			},
+			templates = { "builtin" },
+			task_win = {
+				border = "rounded",
+			},
+		},
+		keys = {
+			{ "<leader>or", "<cmd>OverseerRun<CR>", desc = "Run task" },
+			{ "<leader>ot", "<cmd>OverseerToggle<CR>", desc = "Toggle tasks" },
+		},
+	},
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			"kevinhwang91/promise-async",
+		},
+		event = "BufReadPost",
+		config = function()
+			local ufo = require("ufo")
 
-    end,
-  },
-{
-  "stevearc/overseer.nvim",
-  cmd = {
-    "OverseerRun",
-    "OverseerToggle",
-    "OverseerOpen",
-    "OverseerClose",
-  },
-  opts = {
-    strategy = {
-      "toggleterm",
-    },
-    templates = { "builtin" },
-    task_win = {
-      border = "rounded",
-    },
-  },
-  keys = {
-    { "<leader>or", "<cmd>OverseerRun<CR>", desc = "Run task" },
-    { "<leader>ot", "<cmd>OverseerToggle<CR>", desc = "Toggle tasks" },
-  },
-},
-{
-  "kevinhwang91/nvim-ufo",
-  dependencies = {
-    "kevinhwang91/promise-async",
-  },
-  event = "BufReadPost",
-  config = function()
-    local ufo = require("ufo")
+			ufo.setup({
+				provider_selector = function(_, filetype, _)
+					return { "treesitter", "indent" }
+				end,
+			})
 
-    ufo.setup({
-      provider_selector = function(_, filetype, _)
-        return { "treesitter", "indent" }
-      end,
-    })
+			vim.o.foldcolumn = "1"
+			vim.o.foldlevel = 99
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
 
-    vim.o.foldcolumn = "1"
-    vim.o.foldlevel = 99
-    vim.o.foldlevelstart = 99
-    vim.o.foldenable = true
+			vim.keymap.set("n", "zR", ufo.openAllFolds)
+			vim.keymap.set("n", "zM", ufo.closeAllFolds)
 
-    vim.keymap.set("n", "zR", ufo.openAllFolds)
-    vim.keymap.set("n", "zM", ufo.closeAllFolds)
+			-- peek fold (очень полезно)
+			vim.keymap.set("n", "zp", function()
+				ufo.peekFoldedLinesUnderCursor()
+			end)
+		end,
+	},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {
+			jump = {
+				autojump = true,
+			},
+		},
+		keys = {
+			{
+				"s",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").jump()
+				end,
+				desc = "Flash Jump",
+			},
+			{
+				"S",
+				mode = { "n", "x", "o" },
+				function()
+					require("flash").treesitter()
+				end,
+				desc = "Flash Treesitter",
+			},
+			{
+				"r",
+				mode = "o",
+				function()
+					require("flash").remote()
+				end,
+				desc = "Remote Flash",
+			},
+			{
+				"R",
+				mode = { "o", "x" },
+				function()
+					require("flash").treesitter_search()
+				end,
+				desc = "Treesitter Search",
+			},
+			{
+				"<c-s>",
+				mode = { "c" },
+				function()
+					require("flash").toggle()
+				end,
+				desc = "Toggle Flash",
+			},
+		},
+	},
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = true,
+	},
 
-    -- peek fold (очень полезно)
-    vim.keymap.set("n", "zp", function()
-      ufo.peekFoldedLinesUnderCursor()
-    end)
-  end,
-},
-{
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  opts = {
-    jump = {
-      autojump = true,
-    },
-  },
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Jump" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash" },
-  },
-},
-{
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  config = true,
-},
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre", -- this will only start session saving when an actual file was opened
+		config = function()
+			require("persistence").setup()
+		end,
+		opts = {
+			-- add any custom options here
+		},
+	},
 
-{
-  "folke/persistence.nvim",
-  event = "BufReadPre", -- this will only start session saving when an actual file was opened
-  config = function()
-    require("persistence").setup()
-  end,
-  opts = {
-    -- add any custom options here
-  }
-},
+	{
+		"DrKJeff16/boolean-toggle.nvim",
+		cmd = { "Bool" }, -- Lazy-load by commands
+		opts = {},
+	},
 
-{
-  'DrKJeff16/boolean-toggle.nvim',
-  cmd = { 'Bool' }, -- Lazy-load by commands
-  opts = {},
-},
+	{
+		"smjonas/live-command.nvim",
+		main = "live-command",
+		opts = {
+			commands = {
+				Norm = { cmd = "norm" },
+			},
+		},
+	},
 
-{
-  "smjonas/live-command.nvim",
-  main = "live-command",
-  opts = {
-    commands = {
-      Norm = { cmd = "norm" },
-    },
-  },
-},
+	{
+		"ZhiyuanLck/smart-pairs",
+		event = "InsertEnter",
+		config = function()
+			require("pairs"):setup()
+		end,
+	},
 
-{
-  'ZhiyuanLck/smart-pairs',
-  event = 'InsertEnter',
-  config = function()
-    require('pairs'):setup()
-  end
-},
+	{ "nvim-mini/mini.jump", version = "*" },
 
-{ 'nvim-mini/mini.jump', version = '*' },
+	{
+		"abecodes/tabout.nvim",
+		lazy = false,
+		config = function()
+			require("tabout").setup({
+				tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
+				backwards_tabkey = "<S-Tab>", -- key to trigger backwards tabout, set to an empty string to disable
+				act_as_tab = true, -- shift content if tab out is not possible
+				act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+				default_tab = "<C-t>", -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+				default_shift_tab = "<C-d>", -- reverse shift default action,
+				enable_backwards = true, -- well ...
+				completion = false, -- if the tabkey is used in a completion pum
+				tabouts = {
+					{ open = "'", close = "'" },
+					{ open = '"', close = '"' },
+					{ open = "`", close = "`" },
+					{ open = "(", close = ")" },
+					{ open = "[", close = "]" },
+					{ open = "{", close = "}" },
+				},
+				ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+				exclude = {}, -- tabout will ignore these filetypes
+			})
+		end,
+		dependencies = { -- These are optional
+			"nvim-treesitter/nvim-treesitter",
+			"L3MON4D3/LuaSnip",
+			"hrsh7th/nvim-cmp",
+		},
+		opt = true, -- Set this to true if the plugin is optional
+		event = "InsertCharPre", -- Set the event to 'InsertCharPre' for better compatibility
+		priority = 1000,
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		keys = function()
+			-- Disable default tab keybinding in LuaSnip
+			return {}
+		end,
+	},
 
-  {
-    'abecodes/tabout.nvim',
-    lazy = false,
-    config = function()
-      require('tabout').setup {
-        tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
-        backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
-        act_as_tab = true, -- shift content if tab out is not possible
-        act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
-        default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
-        default_shift_tab = '<C-d>', -- reverse shift default action,
-        enable_backwards = true, -- well ...
-        completion = false, -- if the tabkey is used in a completion pum
-        tabouts = {
-          { open = "'", close = "'" },
-          { open = '"', close = '"' },
-          { open = '`', close = '`' },
-          { open = '(', close = ')' },
-          { open = '[', close = ']' },
-          { open = '{', close = '}' }
-        },
-        ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
-        exclude = {} -- tabout will ignore these filetypes
-      }
-    end,
-    dependencies = { -- These are optional
-      "nvim-treesitter/nvim-treesitter",
-      "L3MON4D3/LuaSnip",
-      "hrsh7th/nvim-cmp"
-    },
-    opt = true,  -- Set this to true if the plugin is optional
-    event = 'InsertCharPre', -- Set the event to 'InsertCharPre' for better compatibility
-    priority = 1000,
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      -- Disable default tab keybinding in LuaSnip
-      return {}
-    end,
-  },
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
 
-{
-  "folke/trouble.nvim",
-  opts = {}, -- for default options, refer to the configuration section for custom setup.
-  cmd = "Trouble",
-  keys = {
-    {
-      "<leader>xx",
-      "<cmd>Trouble diagnostics toggle<cr>",
-      desc = "Diagnostics (Trouble)",
-    },
-    {
-      "<leader>xX",
-      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-      desc = "Buffer Diagnostics (Trouble)",
-    },
-    {
-      "<leader>cs",
-      "<cmd>Trouble symbols toggle focus=false<cr>",
-      desc = "Symbols (Trouble)",
-    },
-    {
-      "<leader>cl",
-      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-      desc = "LSP Definitions / references / ... (Trouble)",
-    },
-    {
-      "<leader>xL",
-      "<cmd>Trouble loclist toggle<cr>",
-      desc = "Location List (Trouble)",
-    },
-    {
-      "<leader>xQ",
-      "<cmd>Trouble qflist toggle<cr>",
-      desc = "Quickfix List (Trouble)",
-    },
-  },
-}
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		branch = "main",
+		init = function()
+			-- Disable entire built-in ftplugin mappings to avoid conflicts.
+			-- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+			vim.g.no_plugin_maps = true
 
+			-- Or, disable per filetype (add as you like)
+			-- vim.g.no_python_maps = true
+			-- vim.g.no_ruby_maps = true
+			-- vim.g.no_rust_maps = true
+			-- vim.g.no_go_maps = true
+			--
+		end,
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				textobjects = {
+					move = {
+						enable = true,
+						set_jumps = true,
+						goto_next_start = {
+							["[f"] = "@function.outer",
+							["]["] = "@class.outer",
+						},
+						goto_previous_start = {
+							["]f"] = "@function.outer",
+							["[["] = "@class.outer",
+						},
+					},
+					select = {
+						enable = true,
+						lookahead = true,
+
+						keymaps = {
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+						},
+					},
+				},
+			})
+		end,
+	},
+	{
+		"Wansmer/treesj",
+		keys = { "<space>m", "<space>j", "<space>s" },
+		dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
+		config = function()
+			require("treesj").setup({--[[ your config ]]
+			})
+		end,
+	},
+	{
+		"kylechui/nvim-surround",
+		version = "^4.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = VeryLazy,
+		-- Optional: See `:h nvim-surround.configuration` and `:h nvim-surround.setup` for details
+		-- config = function()
+		--     require("nvim-surround").setup({
+		--         -- Put your configuration here
+		--     })
+		-- end
+	},
 }
